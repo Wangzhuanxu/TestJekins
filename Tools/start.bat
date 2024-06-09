b9e372c670b3017e43768fd5d8f08f6f1f8708bb
wangzhuanxu <1033428951@qq.com>
2024-06-09 22:49:50 +0800

    init

---
+++ Tools/start.bat
@@ -0,0 +1,6 @@
+"D:\Unity\2018.4.27f1\Unity\Editor\Unity.exe" ^
+-quit ^
+-batchmode ^
+-projectPath "projectTest" ^
+-executeMethod BuildTools.BuildApk  ^
+-logFile "output.log"
---
+++ projectTest/Assets/Editor/BuildTools.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class BuildTools
+{
+    [MenuItem("Build/Build APK")]
+    public static void BuildApk()
+    {
+        BuildPlayerOptions opt = new BuildPlayerOptions();
+        opt.scenes = GetScenePath();
+        opt.locationPathName = "Bin/release.apk";
+        opt.target = BuildTarget.Android;
+        opt.options = BuildOptions.None;
+
+        BuildPipeline.BuildPlayer(opt);
+
+        Debug.Log("Build App Done!");
+    }
+
+    public static string[] GetScenePath()
+    {
+        EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+
+        List<string> ls = new List<string>();
+        foreach (EditorBuildSettingsScene scene in scenes)
+        {
+            ls.Add(scene.path);
+        }
+
+        return ls.ToArray();
+    }
+}
