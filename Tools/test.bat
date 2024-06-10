9909ce89d4b3b0da12b6357e4b9b83712ab48df8
wangzhuanxu <1033428951@qq.com>
2024-06-10 22:19:06 +0800

    test.bat

--- Tools/test.bat
+++ Tools/test.bat
@@ -1,1 +1,34 @@
-echo ttttt
+REM 路径和删除git相关锁
+set PROJ_PATH=projectTest
+set APK_TARGET_FOLDER="/Volumes/BAK/apk_test/"
+set GRADLE_PATH="/opt/gradle-6.7.1/bin/gradle"
+del /f /q ".git/index.lock"
+cd %PROJ_PATH%
+
+
+#清理热更备份目录
+del /f /q "BundleVersions\*"
+
+#删除脚本缓存文件
+set IsRemoveScriptAssembleDirectory=true
+SCRIPT_ASSEMBLE_PATH=%PROJ_PATH%/Library/ScriptAssemblies
+echo %SCRIPT_ASSEMBLE_PATH%
+if %IsRemoveScriptAssembleDirectory%=="true"(
+	if exist "%SCRIPT_ASSEMBLE_PATH%"
+	(
+		echo %SCRIPT_ASSEMBLE_PATH%
+		rmdir /s /q %SCRIPT_ASSEMBLE_PATH%
+	)
+)
+else(
+)
+
+
+# git拉取代码最新
+git checkout -- .
+git clean -df
+git fetch -p
+git checkout $BRANCH
+git pull
+
+echo pullover
