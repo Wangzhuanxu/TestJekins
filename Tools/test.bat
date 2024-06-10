19836d6cd6f10544dd3293ba28916d545dc205d4
wangzhuanxu <1033428951@qq.com>
2024-06-10 23:04:20 +0800

    肚肚

--- Tools/test.bat
+++ Tools/test.bat
@@ -5,12 +5,9 @@ echo %PROJ_PATH%
 set APK_TARGET_FOLDER="/Volumes/BAK/apk_test/"
 set GRADLE_PATH="/opt/gradle-6.7.1/bin/gradle"
 
-if exist "%CUR_DIR%\.git\index.lock"
-(
+if exist "%CUR_DIR%\.git\index.lock" (
 	del /f /q "%CUR_DIR%\.git\index.lock"
-)
-else
-(
+) else (
 	echo git not exit
 }
 
@@ -23,16 +20,12 @@ del /f /q "%CUR_DIR%\BundleVersions\*"
 set IsRemoveScriptAssembleDirectory=true
 set SCRIPT_ASSEMBLE_PATH=%PROJ_PATH%\Library\ScriptAssemblies
 echo %SCRIPT_ASSEMBLE_PATH%
-if %IsRemoveScriptAssembleDirectory%=="true"
-(
-	if exist "%SCRIPT_ASSEMBLE_PATH%"
-	(
+if %IsRemoveScriptAssembleDirectory%=="true" (
+	if exist "%SCRIPT_ASSEMBLE_PATH%" (
 		echo %SCRIPT_ASSEMBLE_PATH%
 		rmdir /s /q %SCRIPT_ASSEMBLE_PATH%
 	)
-)
-else
-(
+) else (
 )
 
 
