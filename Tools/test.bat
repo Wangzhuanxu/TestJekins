edbb69ca4ea48c80651388ac67670326cdd72352
wangzhuanxu <1033428951@qq.com>
2024-06-10 22:55:45 +0800

    hh

--- Tools/test.bat
+++ Tools/test.bat
@@ -1,4 +1,4 @@
-REM 路径和删除git相关锁
+
 SET CUR_DIR=%CD%
 set PROJ_PATH=%CUR_DIR%\projectTest
 echo %PROJ_PATH%
@@ -7,10 +7,10 @@ set GRADLE_PATH="/opt/gradle-6.7.1/bin/gradle"
 del /f /q "%CUR_DIR%\.git\index.lock"
 cd %PROJ_PATH%
 
-#清理热更备份目录
+
 del /f /q "%CUR_DIR%\BundleVersions\*"
 
-#删除脚本缓存文件
+
 set IsRemoveScriptAssembleDirectory=true
 SCRIPT_ASSEMBLE_PATH=%PROJ_PATH%\Library\ScriptAssemblies
 echo %SCRIPT_ASSEMBLE_PATH%
@@ -24,7 +24,6 @@ else(
 )
 
 
-# git拉取代码最新
 git checkout -- .
 git clean -df
 git fetch -p
